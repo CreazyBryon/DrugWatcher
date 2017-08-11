b5ae254783ea7e277fd19de4fbe148fbc33b54fc
Yu Yong <jason.yu4@honeywell.com>
2017-08-11 16:57:45 +0800

    react + webpack

---
+++ src/components/drugWatcher/DrugListComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/drugWatcher/DrugList.css');
+
+class DrugListComponent extends React.Component {
+  render() {
+    return (
+      <div className="druglist-component">
+        Please edit src/components/my/drugWatcher//DrugListComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+DrugListComponent.displayName = 'MyDrugWatcherDrugListComponent';
+
+// Uncomment properties you need
+// DrugListComponent.propTypes = {};
+// DrugListComponent.defaultProps = {};
+
+export default DrugListComponent;
