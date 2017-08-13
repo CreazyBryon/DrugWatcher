2acbe4f33cf7baf485acb0d03e855af90d4ecd7f
jason225 <hsmsyy2003@163.com>
2017-08-13 09:45:56 +0800

    bootstrap,home

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,21 @@
  
 require('styles/App.css');
+require('styles/bootstrap.min.css');
+
 
 import React from 'react';
-import DrugList from './drugWatcher/DrugListComponent';
-import MyCalculator from './drugWatcher/MyCalculator';
+import MainFrame from './drugWatcher/MainFrame2Component';
 
-let yeomanImage = require('../images/yeoman.png');
+
 
 class AppComponent extends React.Component {
   render() {
+	  
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Hello world!</div>
-		<DrugList />
-		<MyCalculator/>
+		  <MainFrame/>
+		
+      
       </div>
     );
   }
--- src/components/drugWatcher/DrugListComponent.js
+++ src/components/drugWatcher/DrugListComponent.js
@@ -4,11 +4,25 @@ import React from 'react';
 
 require('styles/drugWatcher/DrugList.css');
 
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <div className="row">
+        <div className="col-3">{props.DrugName}</div>
+        <div className="col-6">{props.DrugRemark}</div>
+        <div className="col-3">Detail</div>
+      </div>;
+}
+
 class DrugListComponent extends React.Component {
   render() {
+	  const drugList = [{DrugName:'danggui',DrugRemark:'danggui remark2'}, {DrugName:'fuzi',DrugRemark:'fuzi remark'}, 3, 4, 5];
+	    const drugItems = drugList.map((drug,index) =>
+			// Correct! Key should be specified inside the array.
+			<ListItem key={index} DrugName={drug.DrugName} DrugRemark={drug.DrugRemark} />
+		  );
     return (
       <div className="druglist-component">
-        Please edit src/components/my/drugWatcher//DrugListComponent.js to update this component!
+		{drugItems}
       </div>
     );
   }
