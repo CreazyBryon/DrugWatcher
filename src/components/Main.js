79ae1b1c3fed167d48e49b9981435656d1456da6
jason225 <hsmsyy2003@163.com>
2017-08-24 00:27:57 +0800

    add data

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,14 +5,17 @@ require('styles/bootstrap.min.css');
 
 import React from 'react';
 import MainFrame from './drugWatcher/MainFrame2Component';
+//import logo from '../favicon.svg';
 
-
+let yeomanImage = require('../images/yeoman.png');
+//<img src={yeomanImage} alt="Yeoman Generator" />
 
 class AppComponent extends React.Component {
   render() {
 	  
     return (
       <div className="index">
+	  
 		  <MainFrame/>
 		
       
--- src/components/drugWatcher/DrugList2Component.js
+++ src/components/drugWatcher/DrugList2Component.js
@@ -89,6 +89,17 @@ function loadData(handleRes){
 	return res;
 }
 
+const res={retCode:0,data:[
+  {No:1, Name:'麻黄', Remark:'辛、微苦，温。归肺、膀胱经⒈辛温解表⒉宣肺平喘⒊利水消肿', Category:'解表药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'},
+  {No:2, Name:'防风', Remark:'辛、甘，微温。归膀胱、肝、肺、脾经⒈辛温解表⒉除湿止痛⒊祛风止痉⒋透疹止痒', Category:'解表药',Unit:'两', Amount:'15', UpdateTime:'2017-01-01'},
+  {No:3, Name:'薄荷', Remark:'辛，凉。归肺、肝经⒈辛凉解表⒉清利头目⒊利咽透疹⒋疏肝解郁', Category:'解表药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'},
+  {No:4, Name:'桑叶', Remark:'苦、甘，寒。归肝、肺经⒈疏散风热⒉清肺润肺⒊清肝明目', Category:'解表药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'},
+  {No:5, Name:'独活', Remark:'辛、苦，微温。归肾、膀胱经⒈祛风除湿⒉散寒止痛', Category:'祛风湿药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'},
+  {No:6, Name:'藿香', Remark:'辛、微温。归脾、胃、肺经⒈化湿解暑⒉和中止呕⒊辛温解表', Category:'祛湿药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'},
+  {No:7, Name:'石膏', Remark:'辛、甘，大寒。归肺、胃经⒈清热泻火⒉除烦止渴⒊生肌收敛', Category:'清热药',Unit:'两', Amount:'12', UpdateTime:'2017-01-01'}
+  
+]};
+
 class DrugListComponent extends React.Component {
 	constructor(props){
         super(props)
@@ -100,8 +111,8 @@ class DrugListComponent extends React.Component {
         };
 		
 		this.selectDrug = this.selectDrug.bind(this);
-		
-		this.clearSelectedDrug = this.clearSelectedDrug.bind(this); 
+		this.clearSelectedDrug = this.clearSelectedDrug.bind(this);		
+		this.handleAddClick = this.handleAddClick.bind(this); 
     }
 	
 	selectDrug(drugEntity){
@@ -112,16 +123,30 @@ class DrugListComponent extends React.Component {
 		this.setState({selectedDrug:null});
 	}	
 	
+	handleAddClick(){
+		let newDrug={No:0, Name:'new added', Remark:'', Category:'',Unit:'', Amount:'', UpdateTime:''};
+		res.data.push(newDrug);
+		this.setState({list:res.data});
+	}
+	
   render() {
 	let resCtrl=null;
 	if(this.state.selectedDrug){
 		resCtrl=<ItemDetail drug={this.state.selectedDrug} clearClick={this.clearSelectedDrug}/>
 	}else{
 	  //const drugList = [{DrugName:'danggui',DrugRemark:'danggui remark2'}, {DrugName:'fuzi',DrugRemark:'fuzi remark'}, 3, 4, 5];
-	    resCtrl = this.state.list.map((drug,index) =>
+	    
+		let items = this.state.list.map((drug,index) =>
 			// Correct! Key should be specified inside the array.
 			<ListItem key={index} drug={drug} clickHandler={this.selectDrug} />
 		  );
+		  
+		  resCtrl=<div>
+		  <div className="row">
+        <div className="col-12"><a href='#' onClick={this.handleAddClick}>Add</a></div> 
+      </div>
+		  {items}
+		  </div>
 	}
     return (
       <div className="druglist-component">
@@ -141,14 +166,17 @@ class DrugListComponent extends React.Component {
 })
       ,1000
     );
-*/
+
 
 loadData(function(resList){
 						thisHandler.setState({
 						list : resList
 					})
 });
+*/ 
  
+
+this.setState({list:res.data});
     }
 	
 	componentWillUnmount() {
